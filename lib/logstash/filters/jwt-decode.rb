44ca9879ed648fbcb3672be5005f3e9a7cc2f794
Bharat Raj Arutla <v-bharat.a@turvo.com>
2019-05-24 13:39:33 +0530

    Corrected the implementation and added a happy test case

--- lib/logstash/filters/jwt-decode.rb
+++ lib/logstash/filters/jwt-decode.rb
@@ -2,8 +2,8 @@
 require "logstash/filters/base"
 require "jwt"
 
-# This  filter will replace the contents of the default
-# message field with whatever you specify in the configuration.
+# This  filter will decode the jwt token in your message event and retrievs the values
+# as specified in `extract_fields` and adds the extracted values to the event.
 #
 # It is only intended to be used as an .
 class LogStash::Filters::JWTDecode < LogStash::Filters::Base
@@ -11,15 +11,16 @@ class LogStash::Filters::JWTDecode < LogStash::Filters::Base
   # Setting the config_name here is required. This is how you
   # configure this filter from your Logstash config.
   #
-  # filter {
-  #    {
-  #     message => "My message..."
-  #   }
-  # }
+  # jwt-decode {
+  #    "match" => "token",
+  #    "extract_fields" => {"business_id" => "user.busId"}
+  # }    
+  #   
+  # 
   #
   config_name "jwt-decode"
 
-  # Replace the message with this value.
+  # Looks for a match in message which contains the token field
   config :match, :validate => :string, :required => true
   # Supported Algorithms NONE, HMAC, RSASSA and ECDSA
   config :signature_alg, :validate => :string, :required => false, :default => "NONE"
@@ -38,8 +39,21 @@ class LogStash::Filters::JWTDecode < LogStash::Filters::Base
   public
   def filter(event)
     decoded_token = JWT.decode event.get(@match), @key, false, {algorithm: @signature_alg}    
-    @extract_fields.each { |k, v| event.set(:k , decoded_token[:v]) }
+    @extract_fields.each do |k, v| 
+      event.set(k , getValueFromDecodedToken(v, decoded_token[0]))
+    end
     # filter_matched should go in the last line of our successful code
     filter_matched(event)
   end # def filter
+
+  private
+  def getValueFromDecodedToken(key, decoded_token)
+    key.split(".").each do |val, index| 
+          if decoded_token.nil?
+            return nil 
+          end 
+          decoded_token = decoded_token[val]
+    end
+    return decoded_token;   
+  end
 end # class LogStash::Filters::JWTDecode
